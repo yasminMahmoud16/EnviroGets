a32d4cbac7926d06710df74b4ab620a4749a2c18
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2026-07-31 02:46:32 +0300

    adding padding

--- EnviroGets/src/Pages/SoilStudies/SoilStudies.jsx
+++ EnviroGets/src/Pages/SoilStudies/SoilStudies.jsx
@@ -15,121 +15,121 @@ export default function SoilStudies() {
     const soil = t("soil.soilList", { returnObjects: true });
     const soilType = t("soil.soilTypes", { returnObjects: true });
     return (
-      <>
-        <section
-          className="min-h-screen  relative"
-          style={{
-            backgroundImage: `url(${isArabic ? bgSoil : bgSoilEn})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center top",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          <div className="flex flex-col md:flex-row justify-between px-8">
-            {/* container */}
-            <div className="px-8 pt-30  lg:w-3xl pb-2">
-              {soil.map((item, index) => (
-                <div
-                  key={index}
-                  className="flex flex-col justify-center gap-6 mt-2"
-                >
-                  <TitleSec title={item.title} className="text-white" />
+        <>
+            <section
+                className="min-h-screen  relative"
+                style={{
+                    backgroundImage: `url(${isArabic ? bgSoil : bgSoilEn})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center top",
+                    backgroundRepeat: "no-repeat",
+                }}
+            >
+                <div className="flex flex-col md:flex-row justify-between px-8">
+                    {/* container */}
+                    <div className="px-8 pt-30  lg:w-3xl pb-2">
+                        {soil.map((item, index) => (
+                            <div
+                                key={index}
+                                className="flex flex-col justify-center gap-6 mt-2"
+                            >
+                                <TitleSec title={item.title} className="text-white" />
 
-                  <div className="px-8">
-                    <ul className="list-square list-outside">
-                      {item.text.map((text, i) => (
-                        <List key={i} item={text} />
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              ))}
-
-              {soilType.map((item, index) => (
-                <>
-                  <div className="flex flex-col justify-center gap-6 mt-3">
-                    <TitleSec title={item.title} className="text-white" />
-                    <div key={index} className="px-8 ">
-                      <ul className="list-square list-outside">
-                        {item.text.map((text, i) => (
-                          <List key={i} item={text} />
+                                <div className="px-8">
+                                    <ul className="list-square list-outside">
+                                        {item.text.map((text, i) => (
+                                            <List key={i} item={text} />
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
                         ))}
-                      </ul>
+
+                        {soilType.map((item, index) => (
+                            <>
+                                <div className="flex flex-col justify-center gap-6 mt-3">
+                                    <TitleSec title={item.title} className="text-white" />
+                                    <div key={index} className="px-8 ">
+                                        <ul className="list-square list-outside">
+                                            {item.text.map((text, i) => (
+                                                <List key={i} item={text} />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </>
+                        ))}
                     </div>
-                  </div>
-                </>
-              ))}
-            </div>
 
-            {/* images container */}
-            <div className="md:pt-30  flex items-center justify-center lg:block  lg:pr-10  ">
-              <div className={`w-64 ${isArabic ? "pb-15" : "pb-6 "} `}>
-                <img
-                  src={soilImg1}
-                  alt="soil"
-                  className="w-full h-full object-cover "
-                />
-              </div>
+                    {/* images container */}
+                    <div className="md:pt-30  flex items-center justify-center lg:block  lg:pl-10  ">
+                        <div className={`w-64 ${isArabic ? "pb-15" : "pb-6 "} `}>
+                            <img
+                                src={soilImg1}
+                                alt="soil"
+                                className="w-full h-full object-cover "
+                            />
+                        </div>
 
-              <div className="hidden lg:block relative">
-                <div className={`hidden md:block w-64 absolute z-20  `}>
-                  <img
-                    src={soilImg2}
-                    alt="soil"
-                    className="w-full h-full object-cover "
-                  />
+                        <div className="hidden lg:block relative">
+                            <div className={`hidden md:block w-64 absolute z-20  `}>
+                                <img
+                                    src={soilImg2}
+                                    alt="soil"
+                                    className="w-full h-full object-cover "
+                                />
+                            </div>
+                            <div
+                                className={`hidden md:block w-64 absolute ${isArabic ? "left-60 top-30" : "right-30 top-45"}   `}
+                            >
+                                <img
+                                    src={soilImg3}
+                                    alt="soil"
+                                    className="   w-full h-full object-cover"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div
+                    className={`${isArabic ? "hidden lg:block w-60 h-50 absolute bottom-0 left-0" : "hidden "} `}
+                >
+                    <img
+                        src={bottomImg}
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
                 </div>
                 <div
-                  className={`hidden md:block w-64 absolute ${isArabic ? "left-60 top-30" : "right-30 top-45"}   `}
+                    dir=""
+                    className={`${isArabic ? "hidden" : "hidden lg:block w-60 h-50 absolute bottom-0 right-0 "} `}
                 >
-                  <img
-                    src={soilImg3}
-                    alt="soil"
-                    className="   w-full h-full object-cover"
-                  />
+                    <img
+                        src={bottomEng}
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
                 </div>
-              </div>
-            </div>
-          </div>
-
-          <div
-            className={`${isArabic ? "hidden lg:block w-60 h-50 absolute bottom-0 left-0" : "hidden "} `}
-          >
-            <img
-              src={bottomImg}
-              alt=""
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div
-            dir=""
-            className={`${isArabic ? "hidden" : "hidden lg:block w-60 h-50 absolute bottom-0 right-0 "} `}
-          >
-            <img
-              src={bottomEng}
-              alt=""
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div
-            className={`${isArabic ? "hidden" : "hidden lg:block w-30 h-80 absolute bottom-60 right-0  "}`}
-          >
-            <img
-              src={bottomEng2}
-              alt=""
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <div
-            className={`${isArabic ? "hidden lg:block w-40 h-80 absolute bottom-0 left-0 " : "hidden"}`}
-          >
-            <img
-              src={bottomImg2}
-              alt=""
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </section>
-      </>
+                <div
+                    className={`${isArabic ? "hidden" : "hidden lg:block w-30 h-80 absolute bottom-60 right-0  "}`}
+                >
+                    <img
+                        src={bottomEng2}
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div
+                    className={`${isArabic ? "hidden lg:block w-40 h-80 absolute bottom-0 left-0 " : "hidden"}`}
+                >
+                    <img
+                        src={bottomImg2}
+                        alt=""
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+            </section>
+        </>
     );
 }
