c6936750b4ad377abbd17550e84219848ed20e8a
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2026-08-03 23:30:30 +0300

    Fixing performance issue5

--- EnviroGets/vite.config.js
+++ EnviroGets/vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { visualizer } from "rollup-plugin-visualizer";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      open: true,
+      filename: "stats.html",
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   base: "/EnviroGets/",
-
 });
